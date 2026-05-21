8b020e55d53a7ffe629386e9463e89f22c0bd31e
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-05-21 11:27:20 +0530

    changed psots backend and also converted it to prisma instead of mock data

--- src/app/api/comments/[postId]/route.ts
+++ src/app/api/comments/[postId]/route.ts
@@ -1,65 +1,87 @@
-import { comment, post, users } from "@/app/data/mockData";
 import { NextResponse } from "next/server";
+import { Prisma } from "@/lib/prisma";
 
-export async function GET(request : Request, {params}:{params :{postId :string}}){
-    await new Promise(resolve => setTimeout(resolve, 4000));
+export async function GET(
+  request: Request,
+  { params }: { params: { postId: string } },
+) {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  const postId = parseInt(params.postId);
+  if (isNaN(postId)) {
+    return NextResponse.json({ error: "Not Found" }, { status: 400 });
+  }
+  const Post = await Prisma.post.findUnique({
+    where: {
+      id: postId,
+    },
+    include: {
+      comments: {
+        include: {
+          author: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+    },
+  });
+  if (!Post) {
+    return NextResponse.json({ error: "Not Found" }, { status: 404 });
+  }
+  return NextResponse.json({
+    postId: Post.id,
+    postTitle: Post.title,
+    totalComments: Post.comments.length,
+    comments: Post.comments,
+  });
+}
+
+export async function POST(
+  request: Request,
+  { params }: { params: { postId: string } },
+) {
+  try {
     const postId = parseInt(params.postId);
-    if(isNaN(postId)){
-        return NextResponse.json(
-            {error : "Not Found"},
-            {status : 400}
-        )
+    if (isNaN(postId)) {
+      return NextResponse.json({ error: "Invalid Post" }, { status: 400 });
     }
-    const posts = post.find(p => p.id === postId)
-    if(!posts){
-        return NextResponse.json({error : "Not Found"}, {status : 404})
-    }
-    const postComments = comment.filter(comment => comment.postId).map(comment => ({
-        ...comment, author : users.find(user   => user.id === comment.authorId)
-    })).sort((a,b)=> new Date(a.createdAt).getTime())
-    return NextResponse.json({
-        postId,
-        postTitle : posts.title,
-        totalComments : postComments,
-})
-}  
 
-
-export async function POST(request : Request, {params}: {params : {postId : string}}){
-    try{
-        const postId = parseInt(params.postId);
-        if(isNaN(postId)){
-        return NextResponse.json({error : "Invalid Post"}, {status : 400})
-        }
-        const posts = post.find(p => p.id === postId);
-        if(!post){
-            return NextResponse.json({error : "Post not found"}, {status : 404})
-        }
-        const body = await request.json();
-        if(!body.content || !body.authorId){
-            return NextResponse.json(
-                {error : "Content and authorId are required"}, {status : 400}
-            )
-        }
-        const author = users.find(u => u.id === body.authorId)
-        if(!author){
-            return NextResponse.json({error : "Author not found"}, {status : 404})
-        }
-        const newComment = {
-            id : Math.max(...comment.map(c=>c.id))+1,
-            content : body.content,
-            postId : postId,
-            authorId :body.authorId,
-            createdAt : new Date().toISOString(),
-        }
-        comment.push(newComment);
-        const commentWithAuthor = {
-            ...newComment,
-            author : author
-        };
-        return NextResponse.json(commentWithAuthor,{status : 201})
+    const body = await request.json();
+    if (!body.content || !body.authorId) {
+      return NextResponse.json(
+        { error: "Content and authorId are required" },
+        { status: 400 },
+      );
     }
-    catch(error){
-        return NextResponse.json({error : "Something Went wrong"}, {status : 400})
+    const existingPost = await Prisma.post.findUnique({
+        where : {
+            id : postId
+        }
+    })
+    if(!existingPost){
+        return NextResponse.json({
+            error : "Post not found"
+        }, {
+            status : 404
+        })
     }
+    const newComment = 
+        await Prisma.comment.create({
+            data : {
+                content : body.content,
+                postId,
+                authorId : body.authorId
+            },
+            include : {
+                author : true,
+                post : true
+            }
+        })
+    return NextResponse.json(newComment, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Something Went wrong" },
+      { status: 400 },
+    );
+  }
 }
--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,42 +1,34 @@
-import { comment, users, post } from "@/app/data/mockData";
+import { Prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET(request:Request){
-    await new Promise(resolve => setTimeout(resolve,500));
-    const {searchParams} = new URL(request.url);
-    const authorId = searchParams.get('authorId');
-    const postId = searchParams.get("postId");
-    const limit = searchParams.get('limit');
-    let filteredComments = comment
-    if(authorId){
-        const parseAuthorId = parseInt(authorId);
-        if(!isNaN(parseAuthorId)){
-            filteredComments = filteredComments.filter(c => c.id === parseAuthorId )
-        }
-    }
-    if(postId){
-        const parsedPostId = parseInt(postId) ;
-        if(!isNaN(parsedPostId)){
-            filteredComments = filteredComments.filter(c=> c.postId == parsedPostId)
-        }
-    }
-    const commentsWithDetails = filteredComments.map(comment => ({
-    ...comment, author : users.find(user => user.id === comment.authorId),
-    post : post.find(posts => posts.id === comment.postId)
-    }))
-    const sortedComments = commentsWithDetails.sort(
-        (a,b)=>new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-    )
-    let result = sortedComments;
-    if(limit){
-        const parsedLimit= parseInt(limit);
-        if(!isNaN(parsedLimit) && parsedLimit > 0){
-            result = sortedComments.slice(0, parsedLimit)
-        }
-    }
-    return NextResponse.json({
-        total : filteredComments,
-        showing : result,
-        comments : result
-    })
+export async function GET(request: Request) {
+  await new Promise((resolve) => setTimeout(resolve, 500));
+  const { searchParams } = new URL(request.url);
+  const authorId = searchParams.get("authorId");
+  const postId = searchParams.get("postId");
+  const limit = searchParams.get("limit");
+  const parsedLimit = limit ? parseInt(limit) : undefined;
+  const comments = await Prisma.comment.findMany({
+    where: {
+      ...(authorId && {
+        authorId: parseInt(authorId),
+      }),
+      ...(postId && { postId: parseInt(postId) }),
+    },
+      include : {
+    author : true,
+    post : true    
+  },
+  orderBy : {
+    createdAt : "desc"
+  },
+  ...(parsedLimit && {
+    take : parsedLimit
+  })
+
+  })
+  return NextResponse.json({
+    total : comments.length,
+    comments
+  })
 }
