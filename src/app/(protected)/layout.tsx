6e40618f57763e4469fc4d6c30f1456116d87791
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-05-25 19:32:57 +0530

    added protected routing

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,19 @@
+import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation"
+import { headers } from "next/headers"
+
+export default async function ProtectedLayout({
+    children
+} : { 
+    children : React.ReactNode
+}){
+    const session =  await  auth.api.getSession({
+        headers : await headers()
+    })
+    if(!session){
+        redirect('/auth')
+    }
+    return <>
+    {children}
+    </>
+}
