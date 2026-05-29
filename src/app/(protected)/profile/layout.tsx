48d3c16f1a5def9901eb670873fed24794d5b221
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-05-29 10:35:14 +0530

    I have made the frontend UI for security sessions and also fixed few bugs now its time to build backend for security sessions I know I said that I have already made the backend but I just thought why not lets make this app scalable and I am also thinking to make it opensource so I am adding few more stuff to it

--- src/app/(protected)/profile/layout.tsx
+++ src/app/(protected)/profile/layout.tsx
@@ -1,12 +1,12 @@
 import AppDrawer from "./components/AppDrawer";
 import SideBar from "./components/SideBar";
 
-export default function layout({children} : {children : React.ReactNode}){
-    return(
-        <div className="flex min-h-screen">
+export default function layout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex  md:py-22 bg-[#F7F9FB] min-h-screen">
             <SideBar />
             <AppDrawer />
-            <main className="flex-1 bg-[#F7F9FB] p-8 md:ml-[280px] w-full md:my-10">
+            <main className="flex-1  p-8 md:ml-[280px] w-full">
                 {children}
             </main>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "sonner";
 import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
 
 export default function RootLayout({
@@ -18,8 +18,8 @@ export default function RootLayout({
     <html lang="en" className={cn("h-full", "antialiased", "font-sans", geist.variable)}>
       <body className="min-h-screen pt-24 md:pt-10 flex flex-col">
         <LayoutWrapper>
-        {children}
-        <Toaster richColors position="top-right"/>
+          {children}
+          <Toaster richColors position="top-right" />
         </LayoutWrapper>
       </body>
     </html>
